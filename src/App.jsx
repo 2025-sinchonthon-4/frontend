562d8dcad9b1643e180c85f5667c497b2d397851
fkgrkyr <fkgrkyr@gmail.com>
2025-09-05 21:23:46 +0900

    feat: 전체 아이폰 화면 레이아웃 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+// src/App.jsx
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
 import QuizPage from './pages/quiz/QuizPage';
 import MainPage from './pages/main/MainPage';
+import QuizReportPage from './pages/quizReport/quizReportPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/quiz" element={<QuizPage />} />
-      <Route path='/quiz-report' element={<QuizReportPage />} />
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <div className="w-[393px] h-[852px] bg-white shadow-lg overflow-y-auto rounded-3xl border-2 border-gray-300">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+          <Route path='/quiz-report' element={<QuizReportPage />} />
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      width: {
+        'iphone': '393px', // iphone 16 사이즈
+      },
+      height: {
+        'iphone': '852px', // iphone 16 사이즈
+      }
+    },
+  },
   plugins: [],
 };
