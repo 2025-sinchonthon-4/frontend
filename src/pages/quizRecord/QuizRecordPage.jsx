b98c164772bc9f721c41cf32854d42e15d402286
yxpjseo <yjins2004@gmail.com>
2025-09-06 01:48:02 +0900

    style: quiz-record 카드 리스트 완성

--- src/pages/quizRecord/QuizRecordPage.jsx
+++ src/pages/quizRecord/QuizRecordPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button';
 
 function Modal({ open, onClose, title, children }) {
   if (!open) return null;
@@ -51,19 +52,26 @@ const QuizRecordPage = () => {
     "교통 상황과 경유 여부에 따라 달라지지만, 경부고속도로 기준 평균 약 4~5시간이 소요됩니다. 야간/주말에는 더 걸릴 수 있어요.";
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-col">
-        {problem}
-        <div className="flex justify-between">
-          {date}
-          <button className="" onClick={() => setOpen(true)}>
-            정답 및 해설 보기
-          </button>
+    <div className="w-full h-full flex flex-col items-center justify-between bg-[#F5F5F8]">
+
+      <div className="flex flex-col items-center mt-[7rem] w-full h-[33.84rem] overflow-y-auto gap-[1.87rem]">
+        
+        <div className="shrink-0 flex flex-col justify-between items-center w-[19.6875rem] h-[6.4375rem] bg-white rounded-[1rem] shadow-[0_10px_40px_0_rgba(0,0,0,0.03)] p-[1rem]">
+          <div className="w-full truncate text-[0.8125rem] font-semibold mb-[1.13rem]">
+            {problem}
+          </div>
+          <div className="flex justify-between w-full items-center">
+            <span className="text-[0.75rem] text-[#6F6E6E] font-medium">{date}</span>
+            <button className="cursor-pointer h-[1.9rem] w-[6.4375rem] border border-[#6F6E6E] rounded-[1.25rem] text-[#6F6E6E] text-[0.8125rem] font-medium" onClick={() => setOpen(true)}>
+              정답 및 해설
+            </button>
+          </div>
         </div>
+        
       </div>
-      <button className="" onClick={() => navigate("/")}>
+      <Button className='mt-[2.47rem] mb-[7.12rem]' onClick={() => navigate("/")}>
         홈으로 돌아가기
-      </button>
+      </Button>
 
       <Modal open={open} onClose={() => setOpen(false)} title="정답 및 해설">
         <p className="font-semibold">{problem}</p>
