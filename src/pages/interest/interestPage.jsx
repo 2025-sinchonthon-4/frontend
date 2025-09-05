790959d760b068ae0694b72b4daf77c571e7b779
yxpjseo <yjins2004@gmail.com>
2025-09-06 04:39:29 +0900

    fix: interest 세로 스크롤바 삭제

--- src/pages/interest/interestPage.jsx
+++ src/pages/interest/interestPage.jsx
@@ -26,7 +26,7 @@ export default function InterestPage() {
   };
 
   return (
-    <div className="relative w-full h-screen overflow-hidden bg-[#F3F3F6] text-[#0F0F10]">
+    <div className="relative w-full h-full overflow-hidden bg-[#F3F3F6] text-[#0F0F10]">
       {/* 393 × 100vh 박스 (스크롤 방지용, padding 없음) */}
         <InterestIntro onSkip={handleSkip} />
 
