0df1f1055514bf403e568c531962bc85f6fd89d4
fkgrkyr <fkgrkyr@gmail.com>
2025-09-05 22:20:55 +0900

    feat: 로그인 페이지 초안

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,5 +1,20 @@
 const LoginPage = () => {
-  return <div>Login Page</div>;
-}
+  const handleKakaoLogin = () => {
+    // 여기에 카카오 로그인 로직을 구현
+    alert('카카오 로그인 버튼 클릭');
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full p-4 bg-white">
+      {/* 로고 영역 */}
+      <div className="flex items-center justify-center w-64 h-64 mb-15 bg-gray-200 text-gray-700 text-xl font-bold rounded-lg">
+        로고
+      </div>
+
+      {/* 카카오 로그인 버튼 */}
+      <img src="/kakao-login.svg" alt="카카오 로그인" onClick={handleKakaoLogin} />
+    </div>
+  );
+};
 
 export default LoginPage;
