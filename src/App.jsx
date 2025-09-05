00b0bef2a2ee3999e02fc7f00faabc88266a2900
fkgrkyr <fkgrkyr@gmail.com>
2025-09-05 21:11:24 +0900

    environment: 페이지 라우팅, tailwind 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/login/LoginPage';
+import QuizPage from './pages/quiz/QuizPage';
+import MainPage from './pages/main/MainPage';
 
+function App() {
   return (
-    <>
-    
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/quiz" element={<QuizPage />} />
+      <Route path='/quiz-report' element={<QuizReportPage />} />
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
