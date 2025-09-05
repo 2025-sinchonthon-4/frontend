e90cca4d19a0b29d00614ab21cdcbd66c55da41e
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2025-09-06 02:23:58 +0900

    Revert "관심 카테고리 UI"

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 // src/App.jsx
-import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/login/LoginPage";
-import QuizPage from "./pages/quiz/QuizPage";
-import MainPage from "./pages/main/MainPage";
-import QuizReportPage from "./pages/quizReport/quizReportPage";
-import InterestPage from "./pages/interest/interestPage";
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/login/LoginPage';
+import QuizPage from './pages/quiz/QuizPage';
+import MainPage from './pages/main/MainPage';
+import QuizReportPage from './pages/quizReport/quizReportPage';
 
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-200">
-      <div className="w-[393px] h-screen bg-white shadow-lg border-2 border-gray-300">
+      <div className="w-[393px] h-screen bg-white shadow-lg overflow-y-auto border-2 border-gray-300">
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/quiz" element={<QuizPage />} />
-          <Route path="/quiz-report" element={<QuizReportPage />} />
+          <Route path='/quiz-report' element={<QuizReportPage />} />
           <Route path="/" element={<MainPage />} />
-          <Route path="interest" element={<InterestPage />} />
         </Routes>
       </div>
     </div>
