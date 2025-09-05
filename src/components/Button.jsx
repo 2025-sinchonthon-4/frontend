e62bf5731689856ea2e4d2e161b41a65fd6c3ae7
yxpjseo <yjins2004@gmail.com>
2025-09-06 04:19:05 +0900

    style: button 컴포넌트 글씨 굵기 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ children, className = "", ...props }) {
   return (
     <button
       className={"cursor-pointer w-[17rem] h-[2.8125rem] flex justify-center items-center rounded-[1.875rem] bg-[#F79030] " +
-        "text-white text-[1.25rem] font-bold " +
+        "text-white text-[1.25rem] font-semibold " +
         className}
       {...props}
     >
